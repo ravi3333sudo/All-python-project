ac4de679135565bfc9bb8cabcf02c15636d3d29d
ravi3333sudo <raviraushan1313@gmail.com>
2024-11-11 22:45:23 +0530

    Add files via upload

---
+++ Convert PDF to Word and Word to pdf.py
@@ -0,0 +1,22 @@
+from docx2pdf import convert
+
+convert("new.docx","new_pdf.pdf")
+
+
+
+
+
+
+
+
+
+
+
+# from pdf2docx import Converter
+#
+# old_pdf = "css.pdf"
+# new_doc = "new.docx"
+#
+# obj = Converter(old_pdf)
+# obj.convert(new_doc)
+# obj.close()
